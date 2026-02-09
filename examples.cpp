7231d28592994bd7e8f81177bf04bbd0eb4ffee2
Hervé Dutrieux <126701853+hervedutrieux@users.noreply.github.com>
2026-02-09 16:08:22 +0100

    Update examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -92,9 +92,9 @@ void extract_xdep_GPD(double xi, double t, double mu2, double min_x = 1e-4, int 
     PARTONS::List<PARTONS::GPDKinematic> gpdKinematicList;
     int nbre_x(2), nbre_t(2), nbre_Q2(2);
     for(int k=0; k < nbre_t; k++){
-        double t( -8 * k*k / (nbre_t-1) / (nbre_t-1) );
+        double t( -8. * k*k / (nbre_t-1) / (nbre_t-1) );
     for(int j=0; j < nbre_Q2; j++){
-        double q2( pow(2, 1+5*j/(nbre_Q2-1)) );
+        double q2( pow(2., 1.+5.*j/(nbre_Q2-1)) );
     for(int i=0; i < nbre_x; i++){
         double xi (exp(-log(0.0001) / (nbre_x-1) * i + log(0.0001)) );
         gpdKinematicList.add( PARTONS::GPDKinematic(xi, xi, t, q2, q2) );
